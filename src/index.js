f425b4851a578a8e78b6d5e1551979b0152a29fb
SandrinePradier <labelleaffranchie@gmail.com>
2018-08-11 09:53:38 +0200

    update nativialCall and mapboxCall

--- src/index.js
+++ src/index.js
@@ -130,7 +130,7 @@ function parisArrSeeder(){
 
 //2-a buildmatrix function
 
-function buildMatrix(n){
+async function buildMatrix(n){
 	//this function build a matrix, represented with Trajet collection.
 	//it build several trajet with the one commune origin, and all the possible communes destination
 			console.log('launched buildMatrix n°:', n);
@@ -140,8 +140,6 @@ function buildMatrix(n){
 					let communes = resultCommune;
 						let k=0;
 						for (k=0; k<=communes.length; k++){
-								// console.log('n début du for:', n);
-								// console.log('k début du for:', k);
 								createAndSaveTrajet(n,k, communes);
 						}
 				}
@@ -154,7 +152,6 @@ async function createTrajet(a,b, communes){
 			trajet.code = +a+'-'+b;
 			trajet.origin = communes[a];
 			trajet.destination = communes[b];
-	// console.log('trajet:', trajet);
 	return trajet;
 }
 
@@ -192,6 +189,25 @@ async function createAndSaveTrajet(a,b,communes){
 	let saved = await saveTrajet(created);
 }
 
+async function matrixAll(a,b){
+	Commune.find({}, (err, resultCommune) => {
+	if (err){
+		console.log('error in Commune.find when calling buildMatrix')
+	}
+	else{
+		resultCommune.forEach(async(element)=>{
+			let n= resultCommune.indexOf(element);
+			if (n>=a && n<b){
+				let built = await buildMatrix(n);
+			}
+		});
+	}
+});
+}
+
+
+// matrixAll(1,10);
+
 
 
 //2-b Applying buildingMatrix to the whole list of communes
@@ -200,58 +216,65 @@ async function createAndSaveTrajet(a,b,communes){
 //requires too much heap memory for node, so we have amended it so that it creates trajet for one commune at origin 
 //and all other communes at destination.
 // than we make a for loop for several origins.
-// => We need to create a function that slice the list of communes in ranges of several communes.
+// => We need to create a function that slice the fulllist of communes in ranges of several communes.
 // arr.slice(start, end) start included, end excluded;
 // jumpArray will represent an array where rank i will be the start of the slice and i+1 the end of the slice
 // jumpNumber will be the number of communes  of the slice
 
 
 let jumpArray = [0];
-let jumpNumber = 2; //we can test many values. upto 50 is OK for node
+let jumpNumber = 10; //we can test many values. upto 50 is OK for node
 
-// Commune.find({}, (err, resultCommune)=>{
-// 	if (err){
-// 		console.log('error in Commune.find when calling buildMatrix')
-// 	}
-// 	else{
-// 		// 1 - we create the jumpArray from jumpNumber to jumpNumber until the number of communeList
-// 		let communeList = resultCommune;
-// 		let a;
-// 		// do not works processSlice are launched all together, and node crashes
-// 		for (a=0; a<communeList.length-jumpNumber; a=a+jumpNumber){
-// 		// works OK
-// 		// for (a=0; a<10; a=a+jumpNumber){
-// 			let number = a+jumpNumber;
-// 			jumpArray.push(number);
-// 		}
-// 		jumpArray.push(communeList.length);
-// 		console.log('jumpArray:', jumpArray);
-// 		// processArray(jumpArray, communeList);
-// 	}
-// })
-
-
-async function processArray(array, list){
-		console.log('processArray is called');
-		// once one slice has been processed, we will launch another slice, 
-		//until all the sliced have been process ( ie the full list of communes)
-			let i;
-			for (i=0; i<array.length-1; i++){
-				await processSlice(list, i, i+1);
-			}
+function fillJumpArray(){
+	Commune.find({}, (err, resultCommune) => {
+	if (err){
+		console.log('error in Commune.find when calling buildMatrix')
+	}
+	else{
+		// 1 - we create the jumpArray from jumpNumber to jumpNumber until the number of communeList
+		let communeList = resultCommune;
+		let a;
+		for (a=0; a<communeList.length-jumpNumber; a=a+jumpNumber){
+			let number = a+jumpNumber;
+			jumpArray.push(number);
 		}
-
-async function processSlice(list,a,b){
-	//for a range of communes, we will launch the buildMatrix
-		let range = list.slice(jumpArray[a],jumpArray[b]);
-		// console.log('rangelenght:', range.length);
-		console.log('under processing slice: ' +jumpArray[a]+ ' to ' +jumpArray[b]+ '.');
-		let n;
-		for (n=jumpArray[a]; n<jumpArray[b]; n++){
-			console.log('n:', n);
-			// buildMatrix(n);
-		}
+		jumpArray.push(communeList.length);
+		console.log('jumpArray:', jumpArray);
+		return jumpArray;
+	}
+})
 }
+
+// fillJumpArray();
+
+// jumpArray.forEach(async(element)=>{
+// 	let i= jumpArray.indexOf(element);
+// 	console.log('i de jumparray: ', i);
+// 		// await matrixAll(i,i+1);
+// });
+
+
+
+
+// async function processArray(array, list){
+// 		console.log('processArray is called');
+// 		// once one slice has been processed, we will launch another slice, 
+// 		//until all the slices have been processed ( ie the full list of communes)
+// 			let i;
+// 			for (i=0; i<array.length-1; i++){
+// 				await processSlice(list, i, i+1);
+// 			}
+// 		}
+
+// async function processSlice(list,a,b){
+// 	//for a range of communes, we will launch the buildMatrix
+// 		let range = list.slice(jumpArray[a],jumpArray[b]);
+// 		console.log('under processing slice: ' +jumpArray[a]+ ' to ' +jumpArray[b]+ '.');
+// 		let n;
+// 		for (n=jumpArray[a]; n<jumpArray[b]; n++){
+// 			buildMatrix(n);
+// 		}
+// }
 
 //this is not fully finalised. 
 
@@ -273,7 +296,7 @@ function mapboxCall(trajet){
 	let distanceTrajetKm = '';
 	request(urlMapbox, (error, response, body) => {
 		if(error){
-			console.log('error when calling mapbox API')
+			console.log('error when calling mapbox API');
 		}
 		else{
 			let jsonBody = JSON.parse(body);
@@ -289,7 +312,7 @@ function mapboxCall(trajet){
 }
 
 
-//3-b Updating the trajet with the distance and driving time: OK
+// 3-b Updating the trajet with the distance and driving time: OK
 // AModel.findOneAndUpdate(conditions, update, options, callback)
 // options : specify upsert : true => if object not created, it will create it.
 function updateTrajet(trajet, dist, drive){
@@ -307,24 +330,84 @@ function updateTrajet(trajet, dist, drive){
 }
 
 
-//3-c feeding the matrix by calling the function for all trajets
+// 3-c feeding the matrix by calling the function for all trajets
 
-//test for one trajet: OK
-Trajet.findOne({code:'1-2'}, (err, resultTrajet)=>{
+// test for one trajet: OK
+
+Trajet.findOne({code:'2-32'}, (err, resultTrajet) => {
 	if (err){
-		console.log('Trajet to updtae not found')
+		console.log('Trajet to updtae not found');
 	}
 	else {
 		console.log('resultTrajet:', resultTrajet);
 		mapboxCall(resultTrajet);
+		nativiaCall(resultTrajet);
 	}
 })
 
 
-//4RTH STEP *******************************************************
-//feeding the matrix with transport time
+// 4RTH STEP *******************************************************
+// feeding the matrix with transport time using nativia.io API
+// NB: we will select "best" journey (The best journey if you have to display only one.)
+// NB: duration will be retreived in seconds. we will convert duration in minutes.
 
-//4-a Building the function to get the transport time between 2 communes
+
+// exemple de trajet départ arrivé
+// https://6e8bb014-7672-40a6-a747-143e75ced700@api.navitia.io/v1/journeys?from=-122.4752;37.80826&to=-122.402770;37.794682&datetime=20170407T120000
+
+//4-a Building the function to get the transport time for one trajet
+
+function nativiaCall(trajet){
+	console.log('nativiaCall called');
+	let lonO=trajet.origin.coordonnees[0];
+	let latO=trajet.origin.coordonnees[1];
+	let lonD=trajet.destination.coordonnees[0];
+	let latD=trajet.destination.coordonnees[1];
+	// let urlNativia = 'https://'+process.env.NATIVIA_TOKEN'@api.navitia.io/v1/journeys?from='+lonO+';'+latO+'&to='+lonD+';'+latD+'&datetime=20170407T120000';
+	let urlNativia = 'https://'+process.env.NATIVIA_TOKEN+'@api.navitia.io/v1/journeys?from='+lonO+';'+latO+'&to='+lonD+';'+latD;
+	let transportTimeMin = '';
+	request(urlNativia, (error, response, body)=>{
+		if (error){
+			console.log('error when calling api nativia');
+		}
+		else{
+			let bodyjson = JSON.parse(body);
+			// console.log('body.journeys:', bodyjson.journeys);
+			console.log('type:', bodyjson.journeys[0].type);
+			// console.log('duration:', bodyjson.journeys[0].duration);
+			let transportTime = bodyjson.journeys[0].duration;
+			transportTimeMin = transportTime/60;
+			console.log('transportTimeMin:', transportTimeMin);
+			updateTrajetBis(trajet,transportTimeMin);
+		}
+	})
+}
+
+function updateTrajetBis(trajet, transp){
+	let query = {code:trajet.code};
+	let updateDoc = {timeToDestinationTransport:transp};
+	let option = {upsert:true};
+	Trajet.findOneAndUpdate(query,updateDoc,option,(err, resultUpdated) =>{
+		if(err){
+			console.log('error when calling trajet.findOne in updateTrajet');
+		}
+		else{
+			console.log('resultUpdated:', resultUpdated);
+		}
+	})
+}
+
 
 
 //4-b feeding the matrix by calling the function for all documents
+
+//test for one trajet: OK
+// Trajet.findOne({code:'5-3'}, (err, resultTrajet)=>{
+// 	if (err){
+// 		console.log('Trajet to update not found');
+// 	}
+// 	else {
+// 		console.log('resultTrajet:', resultTrajet);
+// 		nativiaCall(resultTrajet);
+// 	}
+// })
